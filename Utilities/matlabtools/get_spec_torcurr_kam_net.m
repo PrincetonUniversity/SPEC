36471e10b818d462e0ecd03a079e4b535811d7ab
Joaquim Loizu <joaquim.loizu@epfl.ch>
2018-11-08 17:18:44 +0100

    Add matlab routines for running SPEC at fixed current (and evaluating the surface currents)

---
+++ Utilities/matlabtools/get_spec_torcurr_kam_net.m
@@ -0,0 +1,53 @@
+function Itor = get_spec_torcurr_kam_net(filename,ntheta)
+
+% Calculates the net toroidal surface-current on each interface
+%
+% INPUT
+%   - filename    : name of hdf5 file
+%   - ntheta      : poloidal resolution for the loop integral
+%
+% OUTPUT
+%   - Itor        : array of size Nvol-1 with values of net-surface-current on each interface (units are mu0*I)
+%
+% Note: Stellarator symmetry assumed in some of the routines used.
+%
+% written by J.Loizu (2017)
+
+
+fdata    = read_spec_field(filename);            
+gdata    = read_spec_grid(filename);
+
+Nvol     = fdata.Nvol;
+Itor     = zeros(1,Nvol-1);
+
+zarr     = 0;
+tarr     = linspace(0,2*pi,ntheta);
+dtheta   = tarr(2)-tarr(1);
+
+sarr     = [1 -1];
+intB     = [0  0];
+
+for ikam=1:Nvol-1
+
+ lvol     = [ikam  ikam+1];
+
+ for i=1:2
+
+  Bcontrav = get_spec_magfield(fdata,lvol(i),sarr(i),tarr,zarr);
+  gmat     = get_spec_metric(gdata,lvol(i),sarr(i),tarr,zarr);
+
+  Bs       = Bcontrav{1};
+  Bt       = Bcontrav{2};
+  Bz       = Bcontrav{3};
+
+  gst      = gmat{1}{2};
+  gtt      = gmat{2}{2};
+  gzt      = gmat{3}{2};
+
+  intB(i)  = trapz(tarr, Bs.*gst + Bt.*gtt + Bz.*gzt);
+
+ end
+
+ Itor(ikam) = intB(2)-intB(1);
+ 
+end
