f2270be2599d0fe90c2b8ac4c8a5807356bc515c
Antoine Baillod <antoine.baillod@epfl.ch>
2019-06-12 17:11:44 +0200

    added output to some matlab routines

--- Utilities/matlabtools/get_spec_beta.m
+++ Utilities/matlabtools/get_spec_beta.m
@@ -1,45 +1,45 @@
-function beta = get_spec_beta(fname)
+function [beta_ax, beta_av] = get_spec_beta(fname, vol_ind)
  
 % Calculates beta of the equilibrium, both the average beta=2*<p>/B(0)^2 
-% and the axis beta=2*p(0)/B(0)^2, and returns the latter
+% and the axis beta=2*p(0)/B(0)^2
 %
 % INPUT
-%   -fname   : path to the hdf5 output file (e.g. 'testcase.sp.h5')
+%  -fname   : path to the hdf5 output file (e.g. 'testcase.sp.h5')
+%  -volumes : Volumes in which the average should be done
 %
 % OUTPUT
-%  -beta     : value of beta on axis
+%  -beta_ax : value of beta on axis
+%  -beta_av : beta average
 %
 % written by J.Loizu (2016) 
 % modified by J.Loizu (05.2017)
+% modified by A.Baillod (06.2019)
 
 
 gdata  = read_spec_grid(fname);
-
 fdata  = read_spec_field(fname);
 
 Nvol   = h5read(fname,'/Nvol');
-
 pscale = h5read(fname,'/pscale');
+press  = pscale*h5read(fname,'/pressure');
 
 volume = zeros(Nvol,1);
 
-press  = pscale*h5read(fname,'/pressure');
+for lvol=1:length(vol_ind)
+    
+ ivol = vol_ind(lvol);
 
-
-for lvol=1:Nvol
-
- volume(lvol) = get_spec_volume(gdata,lvol,64,64,64);
+ volume(lvol) = get_spec_volume(gdata,ivol,64,64,64);
 
 end
 
-avpress = sum(press.*volume)/sum(volume);
+avpress = sum(press(vol_ind).*volume)/sum(volume);
 
 zarr    = linspace(0,2*pi,64);
 modB    = get_spec_modB(fdata,1,-0.99,0,zarr);
 B0      = mean(modB);
 
-beta_ax = 2*press(1)/(B0^2)
+beta_ax = 2*press(1)/(B0^2);
 
-beta_av = 2*avpress/(B0^2)
+beta_av = 2*avpress/(B0^2);
 
-beta    = beta_ax;
