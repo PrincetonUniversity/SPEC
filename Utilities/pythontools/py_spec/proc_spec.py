eff4fe364f9dde2e05393bb7635e2dd702f269f5
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2019-10-24 15:28:49 -0400

    added Poincare plotting routine

--- Utilities/pythontools/py_spec/proc_spec.py
+++ Utilities/pythontools/py_spec/proc_spec.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# # SPEC postprocessing library by K. Aleynikova, ksenia.aleynikova@ipp.mpg.de, 2019
+"""
+SPEC postprocessing library
 
 # Place holder for math introduction of the SPEC data: <br>
 # $R_{ac} cos(m \theta - n \phi)$
 
-
-"""
-@author Ksenia Aleynikova ksenia.aleynikov@ipp.mpg.de
+@author: Ksenia Aleynikova (ksenia.aleynikov@ipp.mpg.de)
+@author: Caoxiang Zhu (czhu@pppl.gov)
+@author: Jonathan Schilling (jonathan.schilling@ipp.mpg.de)
 """
 
 import numpy as _np
@@ -453,34 +453,15 @@ def get_modB(Bcontrav,g):
 #                _surf.plot(zeta=zeta, **kwargs)
 #            surfs.append(_surf)
 #        return surfs
-#    #!/usr/bin/env python3
-#    # -*- coding: utf-8 -*-
-#    """
-#    @author: Jonathan Schilling (jonathan.schilling@ipp.mpg.de)
-#    """
-#
-#    # plot the Poincare data
-#    def plot_spec_poincare(s, toroidalIdx=0, plt=None):
-#        
-#        # extract slice corresponding to the given toroidal cutplane
-#        pltR=s.poincare.R[:,:,toroidalIdx]
-#        pltZ=s.poincare.Z[:,:,toroidalIdx]
-#        
-#        # create a new figure if no object to plot into was given
-#        newFigure = False
-#        if plt == None:
-#            import matplotlib.pyplot as plt 
-#            plt.figure()
-#            newFigure = True
-#        
-#        # do the plot
-#        plt.plot(pltR, pltZ, 'k.', markersize=1)
-#        
-#        # basic decoration if newly created figure
-#        if newFigure:
-#            plt.axis("equal")
-#            plt.xlabel("R / m")
-#            plt.ylabel("Z / m")
-#        
-#        # return the target plot object for further decoration
-#        return plt
+
+
+# plot the Poincare data
+def plot_poincare(s, toroidalIdx=0, plt=None):
+    
+    # extract slice corresponding to the given toroidal cutplane
+    pltR=s.poincare.R[:,:,toroidalIdx]
+    pltZ=s.poincare.Z[:,:,toroidalIdx]
+    
+    # do the plot
+    plt.plot(pltR, pltZ, 'k.', markersize=1)
+
