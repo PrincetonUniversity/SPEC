395b905d4fd9228a7ade5ca230158f1dc9a4a65d
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2019-12-17 08:33:13 +0100

    more global.f90 doxyfication

--- bfield.f90
+++ bfield.f90
@@ -16,8 +16,8 @@
 !! <ul>
 !! <li> The components of the vector potential, \f${\bf A}=A_\theta \nabla + A_\zeta \nabla \zeta\f$, are
 !! \f{eqnarray}{
-!! A_\theta(s,\theta,\zeta) &=& \sum_{i,l} {\color{red}{A_{\theta,e,i,l}}} \; {\overline T}_{l,i}(s) \cos\alpha_i + \sum_{i,l} {\color{magenta}{A_{\theta,o,i,l}}} \; {\overline T}_{l,i}(s) \sin\alpha_i, \label{eq:At} \\
-!! A_\zeta( s,\theta,\zeta) &=& \sum_{i,l} {\color{blue}{A_{\zeta, e,i,l}}} \; {\overline T}_{l,i}(s) \cos\alpha_i + \sum_{i,l} {\color{cyan}{A_{\zeta ,o,i,l}}} \; {\overline T}_{l,i}(s) \sin\alpha_i, \label{eq:Az}
+!! A_\theta(s,\theta,\zeta) &=& \sum_{i,l} {\color{red} {A_{\theta,e,i,l}}} \; {\overline T}_{l,i}(s) \cos\alpha_i + \sum_{i,l} {\color{magenta}{A_{\theta,o,i,l}}} \; {\overline T}_{l,i}(s) \sin\alpha_i, \label{eq:At} \\
+!! A_\zeta( s,\theta,\zeta) &=& \sum_{i,l} {\color{blue}{A_{\zeta, e,i,l}}} \; {\overline T}_{l,i}(s) \cos\alpha_i + \sum_{i,l} {\color{cyan}   {A_{\zeta ,o,i,l}}} \; {\overline T}_{l,i}(s) \sin\alpha_i, \label{eq:Az}
 !! \f}
 !! where \f${\overline T}_{l,i}(s) \equiv \bar s^{m_i/2} \, T_l(s)\f$, \f$T_l(s)\f$ is the Chebyshev polynomial, and \f$\alpha_j \equiv m_j\theta-n_j\zeta\f$.
 !! The regularity factor, \f$\bar s^{m_i/2}\f$, where \f$\bar s \equiv (1+s)/2\f$, is only included if there is a coordinate singularity in the domain
@@ -25,9 +25,9 @@
 !! <li> The magnetic field, \f$\sqrt g \, {\bf B} = \sqrt g B^s {\bf e}_s + \sqrt g B^\theta {\bf e}_\theta + \sqrt g B^\zeta {\bf e}_\zeta\f$, is
 !! \f{eqnarray}{
 !! \begin{array}{ccccrcrcrcrcccccccccccccccccccccccccccccccccccccccccccccccccccc}
-!! \sqrt g \, {\bf B} & = & {\bf e}_s      & \sum_{i,l} [ ( & - m_i {\color{blue}{A_{\zeta, e,i,l}}} & - & n_i {\color{red}{A_{\theta,e,i,l}}} & ) {\overline T}_{l,i}        \sin\alpha_i + ( & + m_i {\color{cyan}{A_{\zeta ,o,i,l}}} & + & n_i {\color{magenta}{A_{\theta,o,i,l}}} & ) {\overline T}_{l,i}        \cos\alpha_i ] \\
-!!                    & + & {\bf e}_\theta & \sum_{i,l} [ ( &                                        & - &     {\color{blue}{A_{\zeta, e,i,l}}} & ) {\overline T}_{l,i}^\prime \cos\alpha_i + ( &                                          & - &     {\color{cyan}{A_{\zeta ,o,i,l}}} & ) {\overline T}_{l,i}^\prime \sin\alpha_i ] \\
-!!                    & + & {\bf e}_\zeta  & \sum_{i,l} [ ( &       {\color{red}{A_{\theta,e,i,l}}} &   &                                      & ) {\overline T}_{l,i}^\prime \cos\alpha_i + ( &       {\color{magenta}{A_{\theta,o,i,l}}} &   &                                        & ) {\overline T}_{l,i}^\prime \sin\alpha_i ]
+!! \sqrt g \, {\bf B} & = & {\bf e}_s      & \sum_{i,l} [ ( & - m_i {\color{blue}{A_{\zeta, e,i,l}}} & - & n_i {\color{red} {A_{\theta,e,i,l}}} & ) {\overline T}_{l,i}        \sin\alpha_i + ( & + m_i {\color{cyan}   {A_{\zeta ,o,i,l}}} & + & n_i {\color{magenta}{A_{\theta,o,i,l}}} & ) {\overline T}_{l,i}        \cos\alpha_i ] \\
+!!                    & + & {\bf e}_\theta & \sum_{i,l} [ ( &                                        & - &     {\color{blue}{A_{\zeta, e,i,l}}} & ) {\overline T}_{l,i}^\prime \cos\alpha_i + ( &                                           & - &     {\color{cyan}   {A_{\zeta ,o,i,l}}} & ) {\overline T}_{l,i}^\prime \sin\alpha_i ] \\
+!!                    & + & {\bf e}_\zeta  & \sum_{i,l} [ ( &       {\color{red} {A_{\theta,e,i,l}}} &   &                                      & ) {\overline T}_{l,i}^\prime \cos\alpha_i + ( &       {\color{magenta}{A_{\theta,o,i,l}}} &   &                                         & ) {\overline T}_{l,i}^\prime \sin\alpha_i ]
 !! \end{array}
 !! \f}
 !! </li>
