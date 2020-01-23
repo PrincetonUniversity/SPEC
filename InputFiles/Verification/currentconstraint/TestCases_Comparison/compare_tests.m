8ef250d39d671fb2305e467fa4b69651d83a2ec1
Antoine Baillod <antoine.baillod@epfl.ch>
2020-01-23 10:53:57 +0100

    minor changes

--- InputFiles/Verification/currentconstraint/TestCases_Comparison/compare_tests.m
+++ InputFiles/Verification/currentconstraint/TestCases_Comparison/compare_tests.m
@@ -9,7 +9,7 @@ tline = fgetl(fid);
 while ischar(tline)
     local_array{ii} = [tline(1:end-2), 'h5'];
     global_array{ii} = [tline(1:6), '3', tline(8:end-2), 'h5'];
-    master_array{ii} = [tline(1:9), '.master', tline(10:end-2), 'h5'];
+    master_array{ii} = [tline(1:9), '.master', tline(10:end-2), 'sp.h5'];
     tline = fgetl(fid);
     ii = ii+1;
 end
