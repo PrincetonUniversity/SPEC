1c7e82d1f44594e79b16fb84a4075591dedcac8c
Antoine Baillod <antoine.baillod@epfl.ch>
2020-01-28 09:35:21 +0100

    minor changes

--- InputFiles/Verification/currentconstraint/TestCases_Comparison/compare_tests.m
+++ InputFiles/Verification/currentconstraint/TestCases_Comparison/compare_tests.m
@@ -19,8 +19,14 @@ for ii = 1:length(local_array)
     
     disp(['COMPARISON OF ', global_array{ii}])
     disp('------------------------------')
+    
+    data1 = read_spec(master_array{ii});
+    data2 = read_spec(global_array{ii});
+    
     try
         compare_spec_outputs(master_array{ii}, global_array{ii});
+        compare_spec_outputs2(data1, data2, 1E-12);
+        disp([newline,' '])
     catch err
         disp(err.message)
     end
@@ -33,8 +39,12 @@ for ii = 1:length(local_array)
     
     disp(['COMPARISON OF ', local_array{ii}])
     disp('------------------------------')
+    data1 = read_spec(master_array{ii});
+    data2 = read_spec(local_array{ii});
     try
         compare_spec_outputs(local_array{ii}, master_array{ii});
+        compare_spec_outputs2(data1, data2, 1E-12);
+        disp([newline,' '])
     catch err
         disp(err.message)
     end
