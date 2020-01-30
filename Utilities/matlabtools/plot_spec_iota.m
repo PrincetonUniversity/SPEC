9f451deae0f7daa8523fd4a3660cf591a3fe4213
Joaquim Loizu <joaquim.loizu@epfl.ch>
2020-01-30 14:09:28 +0100

    Adapted ploting routines for iota. Updated Tutorial files.

--- Utilities/matlabtools/plot_spec_iota.m
+++ Utilities/matlabtools/plot_spec_iota.m
@@ -1,23 +1,28 @@
-function out = plot_spec_iota(idata,pdata,fdata,iorq,xaxis,newfig)
+function out = plot_spec_iota(data,iorq,xaxis,newfig)
 
 % Produces rotational transform plot from field-line-tracing data
 %
 % INPUT
-%   -idata     : must be produced by calling read_spec_iota(filename)
-%   -pdata     : must be produced by calling read_spec_poincare(filename)
-%   -fdata     : must be produced by calling read_spec_field(filename)
+%   -data      : produced by calling read_spec(fname)
 %   -iorq      : plot iota('i') or safety factor ('q')
 %   -xaxis='s' : plots s-coordinnate as the x-axis
 %   -xaxis='R' : plots R-coordinnate as the x-axis
 %   -xaxis='f' : plots toroidal flux as the x-axis
 %   -xaxis='r' : plots sqrt(toroidal flux) as the x-axis
-%   -newfig    : opens(=1) or not(=0) a new figure. (=2 to overwrite 
-%                current figure)
+%   -newfig    : opens(=1) or not(=0) a new figure, or overwrites(=2) current plot
+%
+% OUTPUT
+%   -out       : structure with arrays of iota and the chosen xaxis array
 %
 % written by J.Loizu (2015)
 % modified by J.Loizu (06.2017)
 % debugged by J.Loizu (02.2018)
-% modified by A.Baillod (01.2019) - cylindrical case l.84
+% modified by A.Baillod (01.2019)
+% modified by J.Loizu (01.2020)
+
+pdata = pdata_from_data(data);
+idata = idata_from_data(data);
+fdata = fdata_from_data(data);
 
 if(newfig==1)
     figure
@@ -29,11 +34,9 @@ elseif newfig==2
 end
 
 if(iorq=='i')
-%F = abs(idata.iota(1:end));
 F = idata.iota(1:end);
 Flabel='\iota';
 elseif(iorq=='q')
-%F = 1./abs(idata.iota(1:end));
 F = 1./idata.iota(1:end);
 Flabel='q';
 end
@@ -44,13 +47,17 @@ switch xaxis
  case 's'
   plot(idata.sarr(1:nsucctrj),F(1:nsucctrj),'*','MarkerSize',8,'LineWidth',2)
   ylabel(Flabel)
-  out = 0;
-  
+  out    = cell(2);
+  out{1} = idata.sarr; 
+  out{2} = F;
+
  case 'R'
   plot(transpose(pdata.R_lines(:,1,1)),F(1:nsucctrj),'*','MarkerSize',8,'LineWidth',2)
   ylabel(Flabel)
-  out = 0;
-  
+  out = cell(2);
+  out{1} = pdata.R_lines(:,1,1);            
+  out{2} = F(1:nsucctrj);  
+
  case 'f'
   sval    = idata.sarr(1:nsucctrj);
   nvol    = idata.Mvol;
@@ -90,7 +97,7 @@ switch xaxis
   
   out    = cell(2);
   out{1} = psitor/psitor(end);
-  out{2} = F;
+  out{2} = F(1:nsucctrj);
   
 case 'r'
   sval    = idata.sarr(1:nsucctrj);
@@ -130,6 +137,6 @@ case 'r'
   
   out    = cell(2);
   out{1} = psitor/psitor(end);
-  out{2} = F;
+  out{2} = F(1:nsucctrj);
   
 end
