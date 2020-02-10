e2bc370119ab5af094af41a7f0a328a0e47e65dc
Chris Smiet <chris.smiet@gmail.com>
2020-02-10 15:17:32 -0500

    syntax changes

--- Utilities/pythontools/py_spec/plot/plot_pressure.py
+++ Utilities/pythontools/py_spec/plot/plot_pressure.py
@@ -3,6 +3,7 @@
 # coded by @zhucaoxiang (czhu@pppl.gov)
 # adapted by @smiet (csmiet@pppl.gov)
 
+
 def plot_pressure(SPEC, normalize=True, **kwargs):
     '''Plot stepped pressure profile
     Parameters:
