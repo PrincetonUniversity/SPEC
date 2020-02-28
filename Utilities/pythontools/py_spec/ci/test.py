034d0b5fa7924c98f9c7987d4684aceb316ca2b0
Chris Smiet <chris.smiet@gmail.com>
2020-02-28 18:50:12 -0500

    made test divide by number of elements to make comparison more fair. fixed ci comments for reproducing test made freeboundary test start away from equilibrium

--- Utilities/pythontools/py_spec/ci/test.py
+++ Utilities/pythontools/py_spec/ci/test.py
@@ -23,13 +23,13 @@ tol = args.tol
 match = True
 
 
-def compare(data, reference):
+def compare(data, reference, localtol):
     global match
     for key, value in vars(data).items():
-        if isinstance(value, SPEC):  # recurse data
+        if isinstance(value, SPEC):  # recurse data (csmiet: I'm nt the biggest fan of this recursion...)
             print('------------------')
             print('Elements in '+key)
-            compare(value, reference.__dict__[key])
+            compare(value, reference.__dict__[key], localtol)
         else:
             if key in ['filename', 'version']:  # not compare filename and version
                 continue
@@ -39,21 +39,23 @@ def compare(data, reference):
                 continue
             else:
                 # print(key)
-                diff = np.linalg.norm(np.abs(np.array(value) - np.array(reference.__dict__[key])))
-                unmatch = diff > tol
+                diff = np.linalg.norm(np.abs(np.array(value) - np.array(reference.__dict__[key]))) \
+                        / np.size(np.array(value)) # divide by number of elements
+                unmatch = diff > localtol
                 if unmatch:
                     match = False
                     print('UNMATCHED: '+key, ', diff={:12.5E}'.format(diff))
                 else:
-                    print('ok: '+key)
+                    print('ok: ',key, 'element average difference = {}'.format(diff))
     return
 
 
-compare(data_A, data_B)
+compare(data_A, data_B, tol)
 print('===================')
 if match:
     print('All the terms are within tolerance.')
 else:
+    print('Differences in some elements are larger than the tolerence.')
     raise SystemExit('Differences in some elements are larger than the tolerence.')
 
 
