76412d7d9721d167363afe855dc506941dd8a3b7
Caoxiang Zhu <zcxiang@gmail.com>
2020-12-10 22:55:35 +0800

    import ci.test instead

--- Utilities/pythontools/py_spec/__init__.py
+++ Utilities/pythontools/py_spec/__init__.py
@@ -1,6 +1,6 @@
 # import of all SPEC-related python scripts.
 __version__ = "3.0.1"
 
-from .ci.test import compare
+from .ci import test
 from .input.spec_namelist import SPECNamelist
 from .output.spec import SPECout
