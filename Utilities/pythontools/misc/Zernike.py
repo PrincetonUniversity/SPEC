931698df2a494b93722b1787bf3f5c89d52d20fe
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-09-28 16:38:28 +0200

    refinement of Zernike basis evaluation

--- Utilities/pythontools/misc/Zernike.py
+++ Utilities/pythontools/misc/Zernike.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 Z = 0.0
 m = 10
 n = 20
-r = 0.0
 
 if (n-m)%2==1:
     raise ValueError("n-m is not even!")
@@ -33,31 +32,31 @@ allY = np.zeros([len(allR), len(allTheta)])
 Z2d = np.zeros([len(allR), len(allTheta)])
 
 for idx,r in enumerate(allR):
-        
+
     # j=0: (m,m) entry
     nRangeZ[0] = r**m
-    
+
     # j=1: (m,m+2) entry
     nRangeZ[1] = (m+2) * r**(m+2) - (m+1) * r**m
-    
+
     for j in range(2, int(n_m_2+1)):
         myN = m+2*j
         # print("compute for n="+str(myN)+" at j="+str(j))
-        
+
         prefac = myN/(myN**2 - m**2)
-        
+
         f1_s1 = 4*(myN-1) * r**2
         f1_s2 = (myN-2+m)**2/(myN-2)
         f1_s3 = (myN-m)**2/myN
         f1 = (f1_s1-f1_s2-f1_s3) * nRangeZ[j-1]
-        
+
         f2 = ((myN-2)**2 - m**2)/(myN-2) * nRangeZ[j-2]
-        
+
         nRangeZ[j] = prefac * (f1 - f2)
 
-    
+
     allZ[idx] = nRangeZ[-1]
-    
+
     allX[idx,:] = r*np.cos(allTheta)
     allY[idx,:] = r*np.sin(allTheta)
     Z2d[idx,:] = nRangeZ[-1] * np.cos(m*allTheta)
@@ -68,13 +67,35 @@ print("")
 print("r = "+str(r))
 print(" => Z = "+str(Z))
 
+#%%
 plt.figure()
+
+plt.subplot(2,1,1)
 plt.plot(allR, allZ)
+plt.xlabel("r")
+plt.ylabel("z")
+plt.grid(True)
+plt.title("radial Zernike basis (m=%d, n=%d)"%(m,n))
 
-plt.figure()
+plt.subplot(2,1,2)
 plt.semilogx(1.0-allR, allZ)
+plt.xlabel("log(1-r)")
+plt.ylabel("z")
+plt.grid(True)
 
+plt.tight_layout()
+
+plt.savefig("radialZernikeBasis.png")
+
+#%%
 plt.figure()
-plt.pcolormesh(allX, allY, Z2d)
+plt.pcolormesh(allX, allY, Z2d) # , shading='gouraud')
 plt.axis("equal")
-plt.colorbar()
+cb = plt.colorbar()
+cb.set_label("z")
+plt.xlabel("x = r cos(theta)")
+plt.ylabel("y = r sin(theta)")
+plt.title("m=%d, n=%d"%(m, n))
+plt.tight_layout()
+
+plt.savefig("twoDimZernikeBasis.png")
