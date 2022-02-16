a950d5bcd46d292be39aa8e4d3c0586a5272607c
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-02-16 15:32:37 +0100

    try to get rid of MathJax reload also for gh-pages

--- defaults_mathjax.js
+++ defaults_mathjax.js
@@ -15,9 +15,9 @@ window.MathJax = {
 };
 
 // https://docs.mathjax.org/en/latest/web/configuration.html#configuring-and-loading-in-one-script
-(function () {
-  var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
-  script.async = true;
-  document.head.appendChild(script);
-})();
+//(function () {
+//  var script = document.createElement('script');
+//  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+//  script.async = true;
+//  document.head.appendChild(script);
+//})();
