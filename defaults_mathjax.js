7b7f55e4f0b8523c54904a2f75f5cedd826ad34b
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-02-16 13:42:58 +0100

    try to force MathJax to load on GitHub pages

--- defaults_mathjax.js
+++ defaults_mathjax.js
@@ -1,3 +1,4 @@
+// <script type="text/x-mathjax-config">
 window.MathJax = {
   loader: {
     load: ['[tex]/color', '[tex]/cancel']
@@ -13,3 +14,15 @@ window.MathJax = {
       }
   }
 };
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
+
+// </script>
+// <script type="text/javascript"
+//         src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+// </script>
