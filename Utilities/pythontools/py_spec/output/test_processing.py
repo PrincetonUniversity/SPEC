babfb6043f2e297ff550a11680338527bb8a17f6
Antoine Baillod <antoine.baillod@epfl.ch>
2022-11-24 10:21:13 +0100

    removed useless test

--- Utilities/pythontools/py_spec/output/test_processing.py
+++
@@ -1,8 +0,0 @@
-from py_spec import SPECout
-import numpy as np
-
-d = SPECout('/BIG_14TB/BetaScan/RotatingEllipse/Nfp5/BSmodel1_Pmodel1_Bz002/BetaScan_30/BetaScan_Bz002_run_5.sp.h5')
-
-j_dot_B = d.get_surface_current_density(lvol=np.arange(0,7), nt=32, nz=16)
-
-print(j_dot_B)
