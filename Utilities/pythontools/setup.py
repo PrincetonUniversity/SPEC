63923fec6ff7a46bcdd101bbf47ff1c2cf083dbf
Chris <christopher.smiet@epfl.ch>
2024-07-04 09:43:32 +0200

    moved to pyproject.toml and included py_spec.math

--- Utilities/pythontools/setup.py
+++ Utilities/pythontools/setup.py
@@ -1,28 +1,5 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="py_spec",
-    version="3.3.5",
-    install_requires=["numpy>=1.21.1", 
-                      "f90nml",
-                      "h5py", 
-                      "matplotlib",
-                      "coilpy; python_version<'3.12'",
-                      "scipy>=1.7.0"],
-    description="SPEC(Stepped-Pressure Equilibrium Code) python utilities",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering",
-    ],
-    url="https://princetonuniversity.github.io/SPEC/",
-    author="SPEC developers",
-    license="GNU 3.0",
     packages=['py_spec', 'py_spec.input', 'py_spec.output', 'py_spec.ci', 'py_spec.math']
 )
