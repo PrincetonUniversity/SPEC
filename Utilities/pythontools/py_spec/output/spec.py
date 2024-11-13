34aa10fbaabe0a79d6175887d19de331841a82fb
Philipp Jurasic <philipp.jurasic@tum.de>
2024-11-13 22:16:01 +0100

    simplify scalar handling in SPECout

--- Utilities/pythontools/py_spec/output/spec.py
+++ Utilities/pythontools/py_spec/output/spec.py
@@ -95,15 +95,9 @@ class SPECout:
         if isinstance(_content, h5py.File):
             _content.close()
 
-            # make sure that Lrad is always an array
-            if np.isscalar(self.input.physics.Lrad):
-                self.input.physics.Lrad = np.array([self.input.physics.Lrad])
-            # make sure that im always an array
-            if np.isscalar(self.output.im):
-                self.output.im = np.array([self.output.im])
-            # make sure that in_ is always an array
-            if np.isscalar(self.output.in_):
-                self.output.in_ = np.array([self.output.in_])
+            self.input.physics.Lrad = np.atleast_1d(self.input.physics.Lrad)
+            self.output.im = np.atleast_1d(self.output.im)
+            self.output.in_ = np.atleast_1d(self.output.in_)
 
             # these define the target dimensions in the radial direction
             Nvol = self.input.physics.Nvol
