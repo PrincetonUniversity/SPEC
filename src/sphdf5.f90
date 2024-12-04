58e4e0d85f50db8167277da52c7a422a33e61030
ErolBa <balkovic.erol@gmail.com>
2024-12-04 14:18:40 +0100

    keep up w master 5

--- src/sphdf5.f90
+++ src/sphdf5.f90
@@ -1085,8 +1085,8 @@ subroutine hdfint
   HWRITERA( grpOutput, mn, (Mvol+1), Rbs, iRbs(1:mn,0:Mvol) )
 !latex \type{iZbc(1:mn,0:Mvol)}      & real    & \pb{Fourier harmonics, $Z_{m,n}$, of interfaces} \\
   HWRITERA( grpOutput, mn, (Mvol+1), Zbc, iZbc(1:mn,0:Mvol) )
-!l tex \type{forcetol}               & real    & \pb{force-balance error across interfaces} \\
-!  HWRITERV( grpOutput, 1, forcetol, (/ forcetol /)) ! already in /input/global
+!latex \type{BnsErr} & real & \pb{error in self-consistency of field on plasma boundary (in freeboundary)} \\
+  HWRITERV( grpOutput, 1, BnsErr, (/ BnsErr /)) ! already in /input/global
 !latex \type{ForceErr}               & real    & \pb{force-balance error across interfaces} \\
   HWRITERV( grpOutput,  1, ForceErr, (/ ForceErr /))
 !latex \type{ForceErr}               & real    & \pb{force-balance error across interfaces} \\
